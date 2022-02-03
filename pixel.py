83cf7ac1010193bd7fef81a282fdc591febd93bc
lambert <lambert.fatoux@laposte.net>
2022-02-03 18:23:05 +0100

    added priority and pixel class

---
+++ pixel.py
@@ -0,0 +1,17 @@
+#%%
+from dataclasses import dataclass
+
+
+@dataclass
+class Pixel:
+    value: int
+    x: int
+    y: int
+    confidence: float = 0
+    priority: float = 0
+    data_term: float = 0
+
+    def update_priority(self):
+        self.priority = self.confidence*self.data_term
+    
+# %%
