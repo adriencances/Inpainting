f744ca8278089685fa9ba13e23440a1a97c40076
adriencances <adriencances@gmail.com>
2022-02-14 14:25:51 +0100

    gitignore

---
+++ anciens/brouillon.py
@@ -0,0 +1,217 @@
+from inpainting import *
+import sys
+from numpy import dtype
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib import cm
+from tqdm import tqdm
+from utils import get_patch, rolling_window, load_image, load_mask, get_contour
+
+
+im_number = 8
+
+if len(sys.argv) > 1:
+    nb_iters = int(sys.argv[1])
+else:
+    nb_iters = 1
+
+if len(sys.argv) > 2:
+    patch_size = int(sys.argv[2])
+else:
+    patch_size = 9
+
+def update_conf(pixel_map, update_mask=False):
+    contour = get_contour(mask)
+    if len(contour) == 0:
+        return True
+    for p in contour:
+        x, y = p
+        pixel_map.update_confidence(p)
+        if update_mask:
+            pixel_map.mask[x,y] = 0
+    return False
+
+def show(array, gray=False):
+    if gray:
+        plt.imshow(array, cmap="gray")
+    else:
+        plt.imshow(array)
+    plt.axis("off")
+    plt.show()
+
+def show_contour(im, contour, p_hat, q_hat, p_patch, q_patch, mask, positions, confidence,i, isophotes=None, idx_max=None, normals=None, priorities=None):
+    cmap = cm.get_cmap("gray", 255)
+    new_im = im.copy()
+    # new_im = np.tile(im[:,:,None],3).astype(np.uint8)
+    for p in contour:
+        new_im[p[0], p[1]] = [255,0,0]
+    new_im[mask] = [0,0,255]
+    new_im[tuple(positions)] = [255,255,0]
+    # new_im[p_hat[0]-4:p_hat[0]+5,p_hat[1]-4:p_hat[1]+5] = [0,0,255]
+
+    gs = gridspec.GridSpec(4,2)
+    gs.update(wspace=0.5)
+
+    # fig, ax = plt.subplots(1,2)
+    ax1 = plt.subplot(gs[:,0])
+    ax2 = plt.subplot(gs[:2,1])
+    ax3 = plt.subplot(gs[2:,1])
+
+    ax1.imshow(new_im)
+    ax1.scatter([p_hat[1]], [p_hat[0]], color="m", s=2)
+    ax1.scatter([q_hat[1]], [q_hat[0]], color="m", s=2)
+
+    ax2.imshow(p_patch, cmap=cmap, vmin = 0, vmax = 255)
+    ax2.axis("off")
+    ax3.imshow(q_patch, cmap=cmap, vmin = 0, vmax = 255)
+    ax3.axis("off")
+    # if not priorities is None:
+    #     priority_map = np.zeros(im.shape)
+    #     for p, priority in zip(contour, priorities):
+    #         x1,y1= p
+    #         priority_map[x1,y1] = priority
+    #     imm = ax2.imshow(priority_map)
+    # else:
+    #     imm = ax2.imshow(confidence, interpolation='none')
+
+    # if not isophotes is None:
+    #     for iso, norm, p in zip(isophotes, normals, contour):
+    #         # iso = isophotes[idx_max]
+    #         x1, y1 = p
+    #         if 85< y1 <110:
+    #             ax[0].plot([y1, y1+iso[1]*0.1], [x1, x1 + iso[0]*0.1], color='red')
+    #             ax[0].plot([y1, y1+norm[1]*10], [x1, x1+norm[0]*10], color='green' )
+    # fig.colorbar(imm, ax=ax[1], fraction=0.046, pad=0.04)
+    plt.savefig(f'outputs/{im_number}_{i}')
+    plt.close()
+
+
+
+def show_result(im, mask):
+    cmap = cm.get_cmap("gray", 255)
+    new_im = np.tile(im[:,:,None],3).astype(np.uint8)
+    new_im_masked = np.tile(im[:,:,None],3).astype(np.uint8)
+    new_im_masked[mask] = [255,0,0]
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(new_im_masked)
+    plt.axis("off")
+    plt.subplot(1,2,2)
+    plt.imshow(new_im)
+    plt.axis("off")
+    plt.show()
+
+def show_patches(p_patch, q_patch):
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(p_patch)
+    plt.axis("off")
+    plt.subplot(1,2,2)
+    plt.imshow(q_patch)
+    plt.axis("off")
+    plt.show()
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+
+def get_ssds(pixel_map, p_hat):
+    filter = pixel_map.no_masked_neighbors_filter(pixel_map.mask_init, patch_size=pixel_map.patch_size)
+    filter = filter[(pixel_map.patch_size//2):-(pixel_map.patch_size//2)]
+    filter = filter[:,(pixel_map.patch_size//2):-(pixel_map.patch_size//2)]
+
+    patch = get_patch(p_hat, pixel_map.im_rgb, pixel_map.patch_size)
+    mask_patch = get_patch(p_hat, pixel_map.mask, pixel_map.patch_size)
+    shape = (pixel_map.patch_size, pixel_map.patch_size, 3)
+    windows = rolling_window(pixel_map.im_rgb, shape)
+    windows = windows*np.logical_not(mask_patch[None,None,None,:,:,None])
+
+    ssds = np.sum((windows-patch[None,None,None,:,:,:])**2, axis=(2,3,4,5))
+    ssds[np.logical_not(filter)] = ssds.max()
+    assert ssds.shape == filter.shape
+    # ssds[np.logical_not(filter)] = np.inf
+    return ssds
+
+def show_ssds(pixel_map, p_hat, nb_mins=5):
+    ssds = get_ssds(pixel_map, p_hat)
+    # ssds_plot = np.repeat(ssds[:,:,None], 3, axis=2)
+
+    ind = np.argpartition(ssds.ravel(), nb_mins)[:nb_mins]
+    ind = np.unravel_index(ind, ssds.shape)
+
+    # ssds_plot[ind] = [255,0,0]
+    hps = pixel_map.patch_size//2
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(pixel_map.im_rgb)
+    plt.scatter(ind[1]+hps, ind[0]+hps, s=5, c="red")
+    plt.scatter([p_hat[1]], [p_hat[0]], s=5, c="blue")
+    plt.axis("off")
+    plt.subplot(1,2,2)
+    plt.imshow(ssds)
+    plt.scatter(ind[1], ind[0], s=5, c="red")
+    plt.scatter([p_hat[1]-hps], [p_hat[0]-hps], s=5, c="blue")
+    plt.axis("off")
+    plt.show()
+
+
+
+image_file = f"images/image{im_number}.png"
+mask_file = f"images/mask{im_number}.png"
+im_rgb = load_image(image_file)
+im_rgb = cv2.cvtColor(im_rgb, cv2.COLOR_BGR2RGB)
+mask = load_mask(mask_file)
+print("Image :", im_rgb.shape)
+print("Masque :", mask.shape)
+
+
+im = rgb2gray(im_rgb)
+pixel_map = PixelMap(im, im_rgb, mask, patch_size=patch_size)
+print(f'The patchsize is {pixel_map.patch_size}')
+
+
+# show_ssds(pixel_map, [150,150])
+# show_ssds(pixel_map, [50,150])
+show_ssds(pixel_map, [105,110])
+
+
+# mask_sizes = []
+# mask_size = np.count_nonzero(pixel_map.mask)
+# mask_sizes.append(mask_size)
+# contour_length = []
+# for it in tqdm(range(nb_iters)):
+#     mask_size = np.count_nonzero(pixel_map.mask)
+#     mask_sizes.append(mask_size)
+#     if not it%10:
+#         print(mask_size)
+#     if mask_size == 0:
+#         break
+#     contour = get_contour(pixel_map.mask)
+#     contour_length.append(contour.shape[0])
+#     p_hat, isophotes, idx_max, normals, priorities = pixel_map.get_max_priority_pixel(contour)
+
+#     q_hat, p_patch, q_patch = pixel_map.get_best_patch(p_hat)
+
+#     xmin = max(p_hat[0] - (pixel_map.patch_size//2), 0)
+#     xmax = min(p_hat[0] + (pixel_map.patch_size//2) + 1, pixel_map.height)
+#     ymin = max(p_hat[1] - (pixel_map.patch_size//2), 0)
+#     ymax = min(p_hat[1] + (pixel_map.patch_size//2) + 1, pixel_map.width)
+#     X, Y = np.mgrid[xmin:xmax, ymin:ymax]
+#     positions = np.vstack([X.ravel(), Y.ravel()])
+#     nz_idx = np.nonzero(pixel_map.mask[tuple(positions)])[0]
+#     positions = positions[:,nz_idx]
+
+#     if it >= 0:
+#         show_contour(pixel_map.im_rgb, contour, p_hat, q_hat, p_patch, q_patch, pixel_map.mask, positions, pixel_map.confidence, it, isophotes, idx_max, normals, priorities)
+#     # show_patches(p_patch, q_patch)
+#     pixel_map.copy_image_data(p_hat, q_hat)
+
+
+# # plt.figure()
+# # plt.plot(np.arange(len(contour_length)), contour_length)
+# # plt.title('Nombre de points du contour')
+# # plt.xlabel('Numero iteration')
+# # plt.ylabel('Nb elements contour')
+# # plt.show()
+
+
---
+++ anciens/pixel.py
@@ -0,0 +1,166 @@
+import numpy as np
+from scipy import signal
+from dataclasses import dataclass
+
+from utils import get_patch, rolling_window
+
+
+@dataclass
+class Pixel:
+    value: int
+    x: int
+    y: int
+    confidence: float = 0
+    priority: float = 0
+    data_term: float = 0
+
+    def update_priority(self):
+        self.priority = self.confidence*self.data_term
+
+
+class PixelMap:
+    def __init__(self, im, im_rgb, mask, patch_size=9, alpha=255):
+        self.im = im.copy()
+        self.im[mask > 0] = 0
+        self.im_rgb = im_rgb.copy()
+        self.im_rgb_init = im_rgb.copy()
+        self.im_rgb[mask > 0] = 0
+        self.mask_init = mask.copy()
+        self.mask = mask.copy()
+
+        self.shape = im.shape[:2]
+        self.height, self.width = self.shape
+
+        self.patch_size = patch_size
+        self.alpha = alpha
+
+        self.pixel_map = [[Pixel(im[x,y], x, y) for y in range(self.width)] for x in range(self.height)]
+        self.confidence = np.zeros(self.shape)
+        self.confidence[mask == 0] = 1
+
+        # VOIR SI C'EST REELEMENT UTILE
+        self.gradx, self.grady = np.gradient(self.im)
+
+        self.scharr = np.array([[ -3-3j, 0-10j,  +3 -3j],
+                                [-10+0j, 0+ 0j, +10 +0j],
+                                [ -3+3j, 0+10j,  +3 +3j]])
+
+    def __getitem__(self, x, y):
+        return self.pixel_map[x][y]
+
+    # VERIFIER SI SUBITLITE DANS ARTICLE
+    def update_confidence(self, p):
+        x, y = p
+        confidence_patch = get_patch(p, self.confidence*np.logical_not(self.mask), self.patch_size)
+        self.confidence[x,y] = confidence_patch.mean()
+
+    def no_masked_neighbors_filter(self, mask, patch_size=3):
+        kernel = np.ones((patch_size,patch_size))
+        nb_of_masked_neighbors = signal.convolve2d(mask, kernel, mode='same')
+        filter = (nb_of_masked_neighbors == 0)
+        assert filter.shape == mask.shape
+        return filter
+
+    def compute_isophotes(self, contour):
+        half_patch_size = self.patch_size//2
+        isophotes = []
+        filter = self.no_masked_neighbors_filter(self.mask)
+
+        gradx, grady = np.gradient(self.im)
+        filt_gradx = filter*gradx
+        filt_grady = filter*grady
+        module_grad = filt_gradx**2 + filt_grady**2
+
+        for p in contour:
+            x, y = p
+            patch = get_patch(p, module_grad, self.patch_size)
+            i,j = np.unravel_index(patch.argmax(), patch.shape)
+            xb = x - half_patch_size + i
+            yb = y - half_patch_size + j
+            isophotes.append([-filt_grady[xb,yb], filt_gradx[xb,yb]])
+        return isophotes
+
+    def get_normal(self, p):
+        patch = get_patch(p, self.mask, patch_size=3)
+        grad = -np.sum(patch*self.scharr)
+        gradx = np.real(grad)
+        grady = np.imag(grad)
+
+        normal = np.array([grady, gradx])
+        norm = np.linalg.norm(normal)
+        if norm == 0:
+            return normal
+        normal = normal / norm
+        return normal
+
+    def get_max_priority_pixel(self, contour):
+        priorities = np.zeros(len(contour))
+        normals = []
+        isophotes = self.compute_isophotes(contour)
+        for idx, p in enumerate(contour):
+            x, y = p
+            self.update_confidence(p)
+            normal = self.get_normal(p)
+            normals.append(normal)
+            iso = isophotes[idx]
+            data_term = abs(normal@iso)/self.alpha
+            priorities[idx] = self.confidence[x,y] * data_term
+        idx_max = priorities.argmax()
+        p_hat = contour[idx_max]
+        return p_hat.tolist(), isophotes, idx_max, normals, priorities
+
+    def get_best_patch(self, p_hat):
+        filter = self.no_masked_neighbors_filter(self.mask_init, patch_size=self.patch_size)
+        filter = filter[(self.patch_size//2):-(self.patch_size//2)]
+        filter = filter[:,(self.patch_size//2):-(self.patch_size//2)]
+
+        patch = get_patch(p_hat, self.im_rgb, self.patch_size)
+        mask_patch = get_patch(p_hat, self.mask, self.patch_size)
+        assert patch.shape[2] == 3
+
+        shape = (self.patch_size, self.patch_size, 3)
+        windows = rolling_window(self.im_rgb, shape)
+        assert windows.shape[2] == 1
+        windows = windows*np.logical_not(mask_patch[None,None,None,:,:,None])
+
+        ssds = np.sum((windows-patch[None,None,None,:,:,:])**2, axis=(2,3,4,5))
+        assert ssds.shape == filter.shape
+        ssds[np.logical_not(filter)] = 1e14
+
+        idx_min = np.random.choice(np.flatnonzero(ssds == ssds.min()))
+        q_hat = list(np.unravel_index(idx_min, ssds.shape))
+        q_patch = windows[tuple(q_hat)][0]
+        q_hat[0] += self.patch_size//2
+        q_hat[1] += self.patch_size//2
+        return q_hat, patch, q_patch
+
+    def copy_image_data(self, p_hat, q_hat):
+        assert isinstance(p_hat, list)
+        assert isinstance(q_hat, list)
+
+        xmin = max(p_hat[0] - (self.patch_size//2), 0)
+        xmax = min(p_hat[0] + (self.patch_size//2) + 1, self.height)
+        ymin = max(p_hat[1] - (self.patch_size//2), 0)
+        ymax = min(p_hat[1] + (self.patch_size//2) + 1, self.width)
+        X, Y = np.mgrid[xmin:xmax, ymin:ymax]
+        positions = np.vstack([X.ravel(), Y.ravel()])
+
+        nz_idx = np.nonzero(self.mask[tuple(positions)])[0]
+        positions = positions[:,nz_idx]
+        assert p_hat in positions.transpose(1,0).tolist()
+
+        ref_positions = positions.copy()
+        ref_positions[0] += q_hat[0] - p_hat[0]
+        ref_positions[1] += q_hat[1] - p_hat[1]
+        assert q_hat in ref_positions.transpose(1,0).tolist()
+
+        self.im_rgb[tuple(positions)] = self.im_rgb[tuple(ref_positions)]
+        self.im[tuple(positions)] = self.im[tuple(ref_positions)]
+        self.mask[tuple(positions)] = False
+
+        confidence_value = self.confidence[p_hat[0],p_hat[1]]
+        for p in positions.transpose(1,0):
+            x, y = p
+            self.confidence[x,y] = confidence_value
+
+
